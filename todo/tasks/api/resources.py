bca293db6d32e0c7b8683b15c6d9daf5cc286e56
Quique Porta <quiqueporta@gmail.com>
2018-08-25 13:24:23 +0200

    Add permissions to tasks resource

--- todo/tasks/api/resources.py
+++ todo/tasks/api/resources.py
@@ -1,13 +1,16 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from ..models import Task
 from .serializers import TaskSerializer
+from .permissions import IsOwnerOrDeny
 
 
 class TaskViewSet(viewsets.ModelViewSet):
 
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = (IsAuthenticated, IsOwnerOrDeny)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -123,6 +123,9 @@ STATIC_URL = '/static/'
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100
 }
