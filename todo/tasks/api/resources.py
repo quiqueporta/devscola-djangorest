26520cbb90afd0472e03c02c1ca18fb9c359c9fe
Quique Porta <quiqueporta@gmail.com>
2018-08-25 13:06:39 +0200

    Set request user when creating a task

--- todo/tasks/api/resources.py
+++ todo/tasks/api/resources.py
@@ -8,3 +8,6 @@ class TaskViewSet(viewsets.ModelViewSet):
 
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
