d92df1bc16d668375101c9c22bac220846021fad
Quique Porta <quiqueporta@gmail.com>
2018-08-25 12:59:59 +0200

    Add api for tasks

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from tasks.api.resources import TaskViewSet
+
+
+router = DefaultRouter()
+router.register(r'tasks', viewset=TaskViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/', include(router.urls))
 ]
